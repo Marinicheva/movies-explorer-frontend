775766fc022b435581ac108c3e559be93a6d1206
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-29 23:28:42 +0300

    feat: add header for 1280px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
+  // TODO: temporary const for test different view of header
+  const loggenIn = false;
+
   return (
     <header className='header'>
       <a href='f' className='header__logo-link'>
@@ -11,18 +14,35 @@ const Header = () => {
       </a>
       <nav className='nav'>
         <ul className='nav__list'>
-          <li className='nav__item'>
-            <a href='f' className='nav__link nav__link_active'>Фильмы</a>
-            </li>
-          <li className='nav__item'>
-            <a href='f' className='nav__link'>Сохранённые фильмы</a>
-            </li>
-          <li className='nav__item'>
-            <a href='f' className='nav__link nav__link_dir_account'>Аккаунт</a>
-            </li>
+          {
+            loggenIn && (
+              <>
+                <li className='nav__item'>
+                  <a href='f' className='nav__link nav__link_active'>Фильмы</a>
+                </li>
+                <li className='nav__item'>
+                  <a href='f' className='nav__link'>Сохранённые фильмы</a>
+                </li>
+                <li className='nav__item'>
+                  <a href='f' className='nav__link nav__link_dir_account'>Аккаунт</a>
+                </li>
+              </>
+            )
+          }
+          {
+            !loggenIn && (
+              <>
+                <li className='nav__item'>
+                  <a href="f" className="nav__link nav__link_signup">Регистрация</a>
+                </li>
+                <li className='nav__item'>
+                  <a href="f" className="nav__link nav__link_signin">Войти</a>
+                </li>
+              </>
+            )
+          }
         </ul>
       </nav>
-      
     </header>
   )
 }
