f9d3d704b24f973e553ebd6b493a160c89cd02eb
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-30 22:36:55 +0300

    fix: navtab on mobile and promo on tablets

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,7 +3,7 @@ import './NavTab.css';
 
 const NavTab = () => {
   return (
-    <div>
+    <div className='nav__tab'>
       <a href="f" className="nav__link nav__link_signup">Регистрация</a>
       <a href="f" className="nav__link nav__link_signin">Войти</a>
     </div>
