98ed95533bb869e2103692f963c63026848c2198
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-05 19:29:22 +0300

    feat: add compnents in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,32 @@
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import NotFound from '../NotFound/NotFound';
 import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
 
 import './App.css';
 // TODO: temporary const for test different view of header
-const loggedIn = true;
+const loggedIn = false;
 
 function App() {
 
   return (
     <div className="App">
-      <Header isLoggedIn={loggedIn} />
+      {/* <Header isLoggedIn={loggedIn} /> */}
       <main className='main'>
-        {/* {loggedIn ? <Movies /> : <Main />} */}
-        <Profile />
+        {/* <>
+        {loggedIn ? <Movies /> : <Main />}
+        <Footer />
+        </> */}
+        {/* <Profile /> */}
+        {/* <Register /> */}
+        <Login />
       </main>
-      <Footer />
       {/* <NotFound /> */}
+      
     </div>
   );
 }
