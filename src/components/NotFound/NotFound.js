cf3716b92580806cc4f92e234ade65879a111ba6
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-05 19:28:03 +0300

    refactor: set correct tag

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,11 +4,11 @@ import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <main className="not-found">
+    <section className="not-found">
       <h1 className='not-found__title'>Страница не найдена</h1>
       <p className="not-found__subtitle">404</p>
       <a href="f" className="not-found__back">Назад</a>
-    </main>
+    </section>
   )
 }
 
