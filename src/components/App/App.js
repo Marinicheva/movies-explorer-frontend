be9725764e56fc0a30069c1a50fa14e478a8aa97
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-07 15:47:01 +0300

    refactor: remove Footer in App/

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,6 @@ function App() {
         <Route path='/signup' element={<Register />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-      {/* <Footer /> */}
     </div>
   );
 }
