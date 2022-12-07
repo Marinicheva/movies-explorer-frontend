f22cf002be3d6d2893fae78d81ceee6c91147b1c
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-07 19:13:13 +0300

    feat: add preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
@@ -7,15 +7,23 @@ import Footer from '../Footer/Footer';
 import DB from '../../utils/fakeDB.json';
 
 import "./Movies.css";
+import Preloader from '../Preloader/Preloader';
 
 const Movies = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
 
   return (
     <>
       <Header isLoggedIn={true} />
       <section className='movies'>
         <SearchForm />
-        <MoviesCardList moviesList={DB} showSaveOrDeleteBtn='movie__save-btn'  />
+        { isLoading ? <Preloader /> : <MoviesCardList moviesList={DB} showSaveOrDeleteBtn='movie__save-btn'  /> }
       </section>
       <Footer />
     </>
