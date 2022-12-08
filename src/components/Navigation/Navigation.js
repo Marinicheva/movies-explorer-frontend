a6e4f791f18ab0742edeaa949883601ba5665ace
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-08 10:42:17 +0300

    feat: set favicon and page-title

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,29 @@
+import { useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => {
+
+  const menu = useRef();
+  const hamburger = useRef();
+
+  const handleCloseMenuByOverlay = useCallback((evt) => {
+    if (evt.target === menu.current) {
+      menu.current.classList.remove('nav__wrapper_visible');
+      hamburger.current.classList.remove('hamburger_opened');
+      document.removeEventListener('click', handleCloseMenuByOverlay);
+    }
+  }, []);
+
+  const handleClickHamburger = () => {
+    menu.current.classList.toggle('nav__wrapper_visible');
+    hamburger.current.classList.toggle('hamburger_opened');
+    document.addEventListener('click', handleCloseMenuByOverlay);
+  }
+
   return (
-    <>
-      <nav className='nav'>
+    <div ref={menu} className='nav__wrapper'>
+      <nav  className='nav'>
         <ul className='nav__list'>
           <li className='nav__item'>
             <Link to='/' className='nav__link nav__link_visible_hamburger'>Главная</Link>
@@ -18,13 +37,13 @@ const Navigation = () => {
         </ul>
         <Link to='/profile' className='nav__link nav__account-link'>Аккаунт</Link>
       </nav>
-      <div className='hamburger'>
+      <div ref={hamburger} className='hamburger' onClick={handleClickHamburger}>
         <span></span>
         <span></span>
         <span></span>
       </div>
 
-    </>
+    </div>
 
   )
 }
