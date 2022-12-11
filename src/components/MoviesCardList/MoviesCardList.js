8de7ea58da70d2870147042338fcfd381174a0b8
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-12-11 21:44:16 +0300

    feat: set movies count for render in different screens

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 import './MoviesCardList.css';
 
 const MoviesCardList = ({ moviesList, showSaveOrDeleteBtn }) => {
+  const [deviceWidth, setDeviceWidth] = useState(window.screen.width);
+
+  useEffect(() => {
+    const handleChangeScreenSize = () => {
+      setDeviceWidth(window.screen.width);
+    }
+
+    window.addEventListener('resize', handleChangeScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', handleChangeScreenSize);
+    }
+
+  }, [deviceWidth]);
+
 
   if (!moviesList) {
     return <h2 className='movies__empty-list'>Ничего нет</h2>
   }
 
+  const moviesCount = deviceWidth < 451 ? 5 : deviceWidth < 851 ? 8 : 16;
+  const visibleMovies = moviesCount ? moviesList.slice(0, moviesCount) : moviesList;
+
   return (
     <>
       <ul className='movies__list'>
         {
-          moviesList.map((item) => {
+          visibleMovies.map((item) => {
             return <MoviesCard key={item.id} movieData={item} btnClassName={showSaveOrDeleteBtn} />
           })
         }
